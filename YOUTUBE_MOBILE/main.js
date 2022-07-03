04ac80af088e81da698b0f6ca4a684a2bc5f3fc4
SJ Yoon <64758842+seojin-yoon@users.noreply.github.com>
2022-07-03 20:39:12 +0900

    Delete YOUTUBE_MOBILE directory

--- YOUTUBE_MOBILE/main.js
+++
@@ -1,9 +0,0 @@
-
-const moreBtn = document.querySelector('.info .metadata .moreBtn');
-const title = document.querySelector('.info .metadata .title');
-
-/*if moreBtn clicked or not*/
-moreBtn.addEventListener('click', () => {
-  moreBtn.classList.toggle('clicked');
-  title.classList.toggle('clamp');
-});
